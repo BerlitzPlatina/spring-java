7af48a1dff6251df48ec46056022f0c15ee988a6
Nam Hoang <hainam25101997@gmail.com>
2023-03-06 22:18:51 +0700

    save

---
+++ src/main/java/com/example/constants/ConstantApp.java
@@ -0,0 +1,9 @@
+package com.example.constants;
+
+public final class ConstantApp {
+    public static enum ERole {
+        ROLE_USER,
+        ROLE_MODERATOR,
+        ROLE_ADMIN
+    }
+}
--- src/main/java/com/example/user/User.java
+++ src/main/java/com/example/user/User.java
@@ -9,7 +9,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class User {
-
 	@Id
 	private String userId;
 	private String name;
