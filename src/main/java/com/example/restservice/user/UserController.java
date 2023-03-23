0b23f901a36fe98151039defa611ab7f0703c565
Nam Hoang <hainam25101997@gmail.com>
2023-03-23 21:13:16 +0700

    save

--- src/main/java/com/example/restservice/user/UserController.java
+++ src/main/java/com/example/restservice/user/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/")
 public class UserController {
 
-	private final Logger LOG = LoggerFactory.getLogger(getClass());
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	public UserController(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/user")
     public List<User> getAllUsers() {
@@ -29,10 +31,20 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @PostMapping("/user/{id}")
-    public User createUser(@PathVariable("id") String id, @RequestBody User requestUser) {
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> createUser(@PathVariable("id") String id, @RequestBody User requestUser) {
         LOG.info("Starting my application with {} args.", requestUser.getUsername());
-        return userRepository.findById(id).get();
+        return new ResponseEntity<>(userRepository.findById(id).get(), HttpStatus.OK);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createTutorial(@RequestBody User user) {
+        try {
+            User _user = userRepository.save(new User(user.getUsername(), user.getEmail(), user.getPassword()));
+            return new ResponseEntity<>(_user, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping("/user/{id}")
